2faea56529a49b1cfbc46309ccc0be3448635e00
Daniel Kuntz <dkuntz0@gmail.com>
2017-06-11 16:28:55 -0400

    Initial commit

---
+++ Notella/Notella/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notella
+//
+//  Created by Daniel Kuntz on 6/11/17.
+//  Copyright © 2017 Daniel Kuntz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
